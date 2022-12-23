6a6dca37ddb56fadd1c1d7fb211bc40202559a5e
pd9 <50216580+qazoriginal@users.noreply.github.com>
2022-12-24 00:33:22 +0300

    Update tester.sh

--- lab4/tester.sh
+++ lab4/tester.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-bash $1 1 2 3
+bash "$1" 1 2 3
 ONE=$RANDOM
 TWO=$RANDOM
 THREE=$RANDOM
 FOUR=$RANDOM
 FIVE=$RANDOM
-bash $1 $ONE $TWO $THREE $FOUR $FIVE
-bash $1 foo bar foobar "foo bar"
-bash $1 foo --foo --help -l
+bash "$1" $ONE $TWO $THREE $FOUR $FIVE
+bash "$1" foo bar foobar "foo bar"
+bash "$1" foo --foo --help -l
